8579f44ae2d845dab9885d94ea57e92969dca3bf
Vitor Mattos <vitor.cavalcanti@olxbr.com>
2021-07-12 15:22:58 -0300

    Improves logging

--- cleanup.js
+++ cleanup.js
@@ -2,13 +2,24 @@ const { executeSyncCmd } = require('./utils')
 
 const filterEmpty = x => x
 
+console.log('Listing running containers...')
 const containerIds = executeSyncCmd('docker', ['ps', '-aq']).split('\n').filter(filterEmpty)
 
 /*
+ *console.log('Listing docker images...')
  *const imageIds = executeSyncCmd('docker', ['images', '-aq']).split('\n').filter(filterEmpty)
  *
+ *console.log('Listing docker volumes...')
  *const volumeIds = executeSyncCmd('docker', ['volume', 'ls', '-q']).split('\n').filter(filterEmpty)
  */
+
+console.log('============================================')
+console.log(`Running containers: ${containerIds.length}`)
+/*
+ *console.log(`Total images: ${imageIds.length}`)
+ *console.log(`Total volumes: ${volumeIds.length}`)
+ */
+console.log('============================================\n')
 
 for (let id of containerIds) {
   console.log(`Stopping container ${id}`)
@@ -29,3 +40,5 @@ for (let id of containerIds) {
  *}
  *
  */
+
+ console.log('Finished docker cleanup.')
