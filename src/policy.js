0602b797a60f3a6989ec0577a87dd6c41914b863
Vitor Mattos <vitor.cavalcanti@olxbr.com>
2021-11-17 17:15:51 -0300

    Adds policy for cross-account use of lambda

--- src/policy.js
+++ src/policy.js
@@ -1,11 +1,21 @@
-const buildPolicy = ({ awsPrincipalRules }) => JSON.stringify({
+const AWS_REGION = 'us-east-1';
+
+const buildLambdaPolicy = (awsPrincipalRules) => awsPrincipalRules
+  .map(rule => rule.split(':')[4])
+  .map(id => `arn:aws:lambda:${AWS_REGION}:${id}:function:*`);
+
+const buildPolicy = ({ awsPrincipalRules }) => {
+  const principalRules = JSON.parse(awsPrincipalRules);
+  const lambdaPrincipalRules = buildLambdaPolicy(principalRules);
+
+  return JSON.stringify({
     "Version": "2008-10-17",
     "Statement": [
         {
             "Sid": "AllowPushPull",
             "Effect": "Allow",
             "Principal": {
-                "AWS": JSON.parse(awsPrincipalRules)
+                "AWS": principalRules 
             },
             "Action": [
                 "ecr:GetDownloadUrlForLayer",
@@ -16,8 +26,37 @@ const buildPolicy = ({ awsPrincipalRules }) => JSON.stringify({
                 "ecr:UploadLayerPart",
                 "ecr:CompleteLayerUpload"
             ]
+        },
+        {
+            "Sid": "CrossAccountPermission",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:BatchGetImage",
+                "ecr:GetDownloadUrlForLayer"
+            ],
+            "Principal": {
+                "AWS": principalRules 
+            } 
+        },
+        {
+            "Sid": "LambdaECRImageCrossAccountRetrievalPolicy",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:BatchGetImage",
+                "ecr:GetDownloadUrlForLayer"
+            ],
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Condition": {
+                "StringLike": {
+                    "aws:sourceARN": lambdaPrincipalRules 
+                } 
+            }
         }
     ]
-})
+  })
+}
 
 exports.buildPolicy = buildPolicy
+
