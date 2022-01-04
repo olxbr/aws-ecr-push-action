df69548ceb437abc1791973f55584530d3f8c6f3
Jefferson Braga <jeffbraga@gmail.com>
2022-01-04 19:24:21 -0300

    Feat: Improve Logs

--- src/cleanup.js
+++ src/cleanup.js
@@ -3,7 +3,7 @@ const { executeSyncCmd } = require('./utils')
 const filterEmpty = x => x
 
 export const cleanup = () => {
-  console.log('[INFO] ========== Initializing docker cleanup ==========')
+  console.log('INFO ================ Initializing docker cleanup ================')
   console.log('Listing running containers...')
   const containerIds = executeSyncCmd('docker', ['ps', '-aq']).split('\n').filter(filterEmpty)
 
@@ -19,11 +19,11 @@ export const cleanup = () => {
   if (process.env.GITHUB_RUNNER_LABELS) {
     let dockerSysCmd = "docker system prune -f"
     let dockerRmCmd  = "docker rmi $(docker images | egrep 'ecr|olxbr' | awk '{print $3}')"
-    console.log('[INFO] - Found Self-hosted Runner. Clean all the mess to avoid Ephemeral Storage on k8s...')
-    console.log('[INFO] - Docker System prune: ' + require('child_process').spawnSync(dockerSysCmd,{shell: true}).stdout.toString())
-    console.log('[INFO] - Docker RMI: ' + require('child_process').spawnSync(dockerRmCmd,{shell: true}).stdout.toString())
+    console.log('INFO - Found Self-hosted Runner. Clean all the mess to avoid Ephemeral Storage on k8s...')
+    console.log('INFO - Docker System prune: ' + require('child_process').spawnSync(dockerSysCmd,{shell: true}).stdout.toString())
+    console.log('INFO - Docker RMI: ' + require('child_process').spawnSync(dockerRmCmd,{shell: true}).stdout.toString())
   } else {
-    console.log('[INFO] - No need to clean. Not Self-hosted')
+    console.log('INFO - No need to clean. Not Self-hosted')
   }
 
   /*
@@ -47,5 +47,5 @@ export const cleanup = () => {
   }
   */
 
-  console.log('[INFO] ========== Finished docker cleanup ==========')
+  console.log('INFO ================ Finished docker cleanup ================')
 }
