6a13abc1d96c660c7ea1b9cb0af98a82f8f3080b
Juliano Borba <juliano.sato@protonmail.com>
2022-04-01 10:09:52 -0300

    Add CVEs of Spring Framework RCE vulnerabilities

--- src/enforcedCVEs.js
+++ src/enforcedCVEs.js
@@ -1,7 +1,11 @@
 exports.CVES = [
+    // Log4J Vulnerabilities
     "CVE-2021-44228",
     "CVE-2021-45105",
     "CVE-2021-4104",
     "CVE-2021-45046",
-    "CVE-2019-17571"
+    "CVE-2019-17571",
+    // Spring Cloud Vulnerabilities
+    "CVE-2022-22963",
+    "CVE-2022-22965"
 ];
