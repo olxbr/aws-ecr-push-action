f72e3c3048dc388ebc4cbdea4d0e4d1870bcf08f
Pedro Kiefer <pedro.kiefer@olxbr.com>
2023-11-10 14:57:06 -0300

    fix: use PrincipalOrgID as condition

--- src/policy.js
+++ src/policy.js
@@ -1,54 +1,54 @@
-const AWS_REGION = 'us-east-1';
+const AWS_REGION = "us-east-1";
 
 // Expects a rule in the format [XXXXXXXXXX, ... , ZZZZZZZZZZ]
-const buildPrincipalRulesPolicy = (awsPrincipalRules) => awsPrincipalRules
-  .map(id => `o-${id}`);
+const buildPrincipalRulesPolicy = (awsPrincipalRules) =>
+  awsPrincipalRules.map((id) => `o-${id}`);
 
 const buildPolicy = ({ awsPrincipalRules }) => {
-  const strAWSPrincipalRules = JSON.parse(awsPrincipalRules)
+  const strAWSPrincipalRules = JSON.parse(awsPrincipalRules);
   const principalRules = buildPrincipalRulesPolicy(strAWSPrincipalRules);
 
   return JSON.stringify({
-    "Version": "2008-10-17",
-    "Statement": [
+    Version: "2008-10-17",
+    Statement: [
       {
-        "Sid": "AllowPull",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
+        Sid: "AllowPull",
+        Effect: "Allow",
+        Principal: "*",
+        Action: [
           "ecr:BatchCheckLayerAvailability",
           "ecr:BatchGetImage",
           "ecr:DescribeImages",
           "ecr:DescribeRepositories",
-          "ecr:GetDownloadUrlForLayer"
+          "ecr:GetDownloadUrlForLayer",
         ],
-        "Condition": {
-          "StringLike": {
-            "aws:ResourceOrgID": principalRules
-          }
-        }
+        Condition: {
+          StringLike: {
+            "aws:PrincipalOrgID": principalRules,
+          },
+        },
       },
       {
-        "Sid": "AllowSecImageScanning",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-            "ecr:BatchGetRepositoryScanningConfiguration",
-            "ecr:DescribeImageScanFindings",
-            "ecr:DescribeRepositories",
-            "ecr:GetRegistryScanningConfiguration",
-            "ecr:PutImageScanningConfiguration",
-            "ecr:PutRegistryScanningConfiguration",
-            "ecr:StartImageScan"
+        Sid: "AllowSecImageScanning",
+        Effect: "Allow",
+        Principal: "*",
+        Action: [
+          "ecr:BatchGetRepositoryScanningConfiguration",
+          "ecr:DescribeImageScanFindings",
+          "ecr:DescribeRepositories",
+          "ecr:GetRegistryScanningConfiguration",
+          "ecr:PutImageScanningConfiguration",
+          "ecr:PutRegistryScanningConfiguration",
+          "ecr:StartImageScan",
         ],
-        "Condition": {
-            "StringLike": {
-              "aws:ResourceOrgID": principalRules
-            }
-          }
-    }
-    ]
-  })
-}
+        Condition: {
+          StringLike: {
+            "aws:PrincipalOrgID": principalRules,
+          },
+        },
+      },
+    ],
+  });
+};
 
-exports.buildPolicy = buildPolicy
+exports.buildPolicy = buildPolicy;
